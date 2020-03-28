b4408f747d57519563f0f205b6350b80adac0bee
Imane <imaneelmouradi98@gmail.com>
2020-03-28 13:52:36 +0100

    add: header

---
+++ src/components/layout/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+import "../../css/layout/Header.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <>
+        <nav className="navbar navbar-expand-sm bg-dark navbar-dark justify-content-end">
+          <a className="navbar-brand" href="#">
+            Forever<em className="text-success">BIO </em>: Product Management
+          </a>
+          <button className="btn btn-success ml-auto mr-1">
+            <i className="fas fa-user-circle"></i>
+          </button>
+          <button className="btn btn-success  mr-1">
+            <i className="fas fa-sign-out-alt"></i>
+          </button>
+        </nav>
+      </>
+    );
+  }
+}
+
+export default Header;
