cd6fb0787a1d0b6d0778aedfdec5f39537f76227
Imane <imaneelmouradi98@gmail.com>
2020-05-25 19:44:49 +0000

    update: add articles to admin dash

--- src/components/body/Home/DashboardAdmin.js
+++ src/components/body/Home/DashboardAdmin.js
@@ -11,6 +11,7 @@ import products from "../../../images/products.jpg";
 import users from "../../../images/users.png";
 import categories from "../../../images/categories.jpg";
 import fournisseurs from "../../../images/fournisseurs.jpg";
+import articles from "../../../images/article.png";
 
 export default class DashboardAdmin extends Component {
   render() {
@@ -24,28 +25,34 @@ export default class DashboardAdmin extends Component {
         </h3>
 
         <div className="row container-dash">
-          <div className="col-3 center-block text-center">
+          <div className="col-4 center-block text-center">
             <Link to="/products">
               <img alt="" src={products} />
               <p className="text-dashboard">Produits</p>{" "}
             </Link>
           </div>
-          <div className="col-3 center-block text-center">
+          <div className="col-4 center-block text-center">
             <Link to="/users">
               <img alt="" src={users} />
               <p className="text-dashboard">Utilisateurs</p>
             </Link>
           </div>
-          <div className="col-3 center-block text-center">
+          <div className="col-4 center-block text-center">
             <Link to="/categories">
               <img alt="" src={categories} />
               <p className="text-dashboard">Catégories</p>
             </Link>
           </div>
-          <div className="col-3 center-block text-center">
+          <div className="col-4 center-block text-center">
             <Link to="/fournisseurs">
               <img alt="" src={fournisseurs} />
               <p className="text-dashboard">Fournisseurs</p>
+            </Link>
+          </div>
+          <div className="col-4 center-block text-center">
+            <Link to="/articles">
+              <img alt="" src={articles} />
+              <p className="text-dashboard">Articles</p>
             </Link>
           </div>
         </div>
